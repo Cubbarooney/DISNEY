826c300f8ada38678912a03dc2c62a05325c7366
Cubbarooney <Cubby@knights.ucf.edu>
2022-06-03 12:14:07 -0700

    Started Problem 1 - Amazon Automation
    Created a new project in Visual Studio 2015 to create tests against
    https://www.amazon.com
    
    Uses NUnint, Selenium, and GeckoDriver (Firefox)
    
    Depends on GeckoDriver to be installed on the path
    
    TODO (if time): Try/Catch on using the GeckoDriver

--- README.md
+++ README.md
@@ -2,9 +2,15 @@
 Disney Interview Software (rest of acronym to come later)
 
 ## Problem 1
-Amazon Search, Cart, and Logon test
+Amazon Client Tester
 
-To-Do: All of it
+Opens a browser (Firefox), navigates to https://www.amazon.com, and does automated tests at the website.
+
+Currently the only test is navigating to the website.
+**N.B.**: This project requires that GeckoDriver is installed on the PATH. GeckoDriver can be downloaded here: https://github.com/mozilla/geckodriver/releases
+
+To-Do: All of the prescribed tests
+To-Do (if time permits): Add a Try/Catch arround using the GeckoDriver. Possibly use other browsers, or just straight-up fail the tests requiring GeckoDriver. 
 
 ## Problem 2
 Bunny Hopper
@@ -18,3 +24,4 @@ python BunnyHopper.py
 (Assumes 'python' is on the path)
 
 To-Do: Pass in value instead of just using hardcoded 1 - 6
+To-Do: Per email w/ Poojita, the function should return the number of possible paths
---
+++ problem1/AmazonClientTester/AmazonClientTester/InterviewTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium;
+
+namespace AmazonClientTester
+{
+    [TestFixture]
+    public class InterviewTests
+    {
+        string _url = "https://www.amazon.com";
+        string _search = "Steam Train";
+
+        IWebDriver _driver;
+
+        [SetUp]
+        public void OpenBrowser()
+        {
+            _driver = new FirefoxDriver();
+            _driver.Manage().Window.Maximize();
+        }
+
+        /// <summary>
+        /// Test: Simply open the browser to Amazon, wait 2 seconds, and then
+        /// end the test. Serves as an alternative to "Hello, World!"
+        /// Fail Condition: Only if an error is thrown
+        /// </summary>
+        [Test]
+        public void OpenAmazon()
+        {
+            _driver.Url = _url;
+
+            System.Threading.Thread.Sleep(2000); //2secs
+        }
+
+        [TearDown]
+        public void CloseBrowser()
+        {
+            if(_driver != null)
+            {
+                _driver.Quit();
+            }
+        }
+    }
+}
